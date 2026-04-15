db6876449e8a2910ed85028134a3e1d2a842ec7f
Yoexer Elias Moran Urcia <yoexereliasmoranurcia@gmail.com>
2026-04-14 22:50:06 -0500

    Instalación de vite y tailwind

---
+++ src/ui/Header/index.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+
+    return (
+        <div>
+            <header className="header bg-white dark:bg-black">
+                <h1 className="header__title text-black dark:text-white">Todo List</h1>
+            </header>
+        </div>
+        
+    );
+}
+
+export default Header;
